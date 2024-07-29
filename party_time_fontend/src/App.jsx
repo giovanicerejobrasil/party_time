147c19cf512f555eef0a5dfbffcb48ebae04aac2
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2024-07-29 17:25:25 -0300

    principal jsx project

---
+++ party_time_fontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import { ToastContainer } from "react-toastify";
+
+import "./App.css";
+import "react-toastify/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
